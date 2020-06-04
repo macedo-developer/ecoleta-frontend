d312cf466bf6ded6069abcb21761ba6ad7dba2f9
macedo-developer <macedorenata.developer@gmail.com>
2020-06-04 17:14:13 -0300

    Building Home HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import Home from "./pages/Home";
+
 function App() {
-  return <h1>Hello Word</h1>;
+  return <Home></Home>;
 }
 
 export default App;
