f49f489d97780f79c79d556f46d2006fe20b71fa
macedo-developer <macedorenata.developer@gmail.com>
2020-06-04 22:35:11 -0300

    Searching IBGE states and cities

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
 
 import { Map, TileLayer, Marker } from "react-leaflet";
@@ -9,7 +9,72 @@ import "./styles.css";
 
 import logo from "../../assets/logo.svg";
 
+import axios from "axios";
+import api from "../../services/api";
+
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  const [selectedUf, setSelectedUf] = useState("0");
+  const [selectedCity, setSelectedCity] = useState("0");
+
+  useEffect(() => {
+    api.get("/items").then((response) => setItems(response.data));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla);
+
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedUf === "0") return;
+
+    axios
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((response) => {
+        const cityNames = response.data.map((city) => city.nome);
+
+        setCities(cityNames);
+      });
+  }, [selectedUf]);
+
+  function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+    const uf = event.target.value;
+    setSelectedUf(uf);
+  }
+
+  function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
+    const city = event.target.value;
+
+    setSelectedCity(city);
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -64,15 +129,35 @@ const CreatePoint = () => {
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado</label>
-              <select name="uf" id="uf">
+              <select
+                onChange={handleSelectUf}
+                value={selectedUf}
+                name="uf"
+                id="uf"
+              >
                 <option value="0">Selecione uma opção</option>
+                {ufs.map((uf) => (
+                  <option key={uf} value={uf}>
+                    {uf}
+                  </option>
+                ))}
               </select>
             </div>
 
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select name="city" id="city">
+              <select
+                onChange={handleSelectCity}
+                value={selectedCity}
+                name="city"
+                id="city"
+              >
                 <option value="0">Selecione uma opção</option>
+                {cities.map((city) => (
+                  <option key={city} value={city}>
+                    {city}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
@@ -85,22 +170,12 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img src="http://localhost:3333/upload/oleo.svg" alt="" />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li className="selected">
-              <img src="http://localhost:3333/upload/eletronicos.svg" alt="" />
-              <span>Eletônicos</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/upload/organicos.svg" alt="" />
-              <span>Orgânicos</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/upload/lampadas.svg" alt="" />
-              <span>Lâmpadas</span>
-            </li>
+            {items.map((item) => (
+              <li key={item.id}>
+                <img src={item.image_url} alt="" />
+                <span>{item.title}</span>
+              </li>
+            ))}
           </ul>
         </fieldset>
 
